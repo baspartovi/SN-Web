eb70a3fbae268386e495bf724e834fb422dc5e69
baspartovi <baspartovi@gmail.com>
2016-01-16 14:16:50 -0500

    Add comments and info. Clean up code.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,33 +5,47 @@
  * @name snWebApp
  * @description
  * # snWebApp
- *
+ * Created by Bas Partovi 2015
  * Main module of the application.
  */
 angular
-  .module('snWebApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'plangular'
-  ])
-  .config(function ($routeProvider, plangularConfigProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    plangularConfigProvider.clientId = '5fe8f9dee33cffbd9d2ca2b6708641f1';
-  });
+    .module('snWebApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'plangular'
+    ])
+    .config(function ($routeProvider, plangularConfigProvider) {
+        $routeProvider
+              .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+              })
+              .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+              })
+              .otherwise({
+                redirectTo: '/'
+              });
+        plangularConfigProvider.clientId = '5fe8f9dee33cffbd9d2ca2b6708641f1';
+    })
+    .factory("Instagram", ['$http', function($http) {
+        var clientId = "f7957cfe23274aa99b75f180c2a82cb4";
+        var userIdShanNash = "301474369";
+        return {
+            fetchRecentPosts: function(callback){
+                //var endpoint = "https://api.instagram.com/v1/users/" + userIdBasp + "/media/recent/?";
+                var endpoint = "https://api.instagram.com/v1/users/" + userIdShanNash + "/media/recent/?&client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK";
+                //var endpoint2 = "https://api.instagram.com/v1/media/popular?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK";
+                $http.jsonp(endpoint).success(function(response){
+                    callback(response.data);
+                });
+            }
+        };
+    }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,10 +4,16 @@
  * @ngdoc function
  * @name snWebApp.controller:MainCtrl
  * @description
+ * Created by Bas Partovi 2015
  * # MainCtrl
  * Controller of the snWebApp
  */
 angular.module('snWebApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, Instagram) {
+    $scope.items = [];
+    $scope.data = {};
 
+    Instagram.fetchRecentPosts(function(data){
+      $scope.items = data;
+    });
   });
