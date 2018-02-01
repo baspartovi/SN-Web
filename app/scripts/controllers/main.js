d78d90e5f0de938cacbceaea7ce385eb9eaf69f5
Bas Partovi <baspartovi@gmail.com>
2018-01-31 22:44:29 -0500

    add new playlist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,12 @@
  * Controller of the snWebApp
  */
 angular.module('snWebApp')
-    .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope) {
 
-    });
+      $scope.shanNashTracksUrl = "https://soundcloud.com/shannash/sets/shan-nash-music";
+      $scope.shanNashPlusHouse = "https://soundcloud.com/shannash/sets/plushouse-show";
+
+      $scope.showPlayerOne = true;
+      $scope.playerUrl = $scope.shanNashTracksUrl;
+
+  });
